af5156242d30bc33a5b03e32c9ed4f0f57c7fee2
Nut <006876@g-able.com>
2020-02-20 18:41:46 +0700

    update components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './pages/form/form.component';
+import { TypoComponent } from './pages/typo/typo.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    TypoComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
