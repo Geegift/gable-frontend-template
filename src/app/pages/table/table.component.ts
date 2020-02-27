4ac60f230f92f58e89fa44afed6644528241f844
Krishna <krishna.d@g-able.com>
2020-02-27 13:39:59 +0700

    Data Table
    Add Search and Pagination

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+declare var $;
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent implements OnInit {
+export class TableComponent implements AfterViewInit {
+  @ViewChild('dataTable') table;
+  dataTable: any;
+  dtOptions: any;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngAfterViewInit() {
+    this.dtOptions = {"columnDefs": [{ 
+      "pagingType": "full_numbers"
+    }]};
+    $(()=>{  
+      $('table.display').DataTable(this.dtOptions);
+    });   
+    this.dataTable = $(this.table.nativeElement);
+    this.dataTable.DataTable(this.dtOptions);
+ }
+
+//   ngOnInit(): void {
+//     this.dtOption = {
+//       "paging": true,
+//       "ordering": false,
+//       "info": false
+//     };   
+//     this.dataTable = $(this.table.nativeElement);
+//     this.dataTable.DataTable(this.dtOption);
+//  }
 
 }
