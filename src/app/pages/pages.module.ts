9ab49790c5d92f1875c7c0311c4648fd9d437a51
Krishna <krishna.d@g-able.com>
2020-03-06 09:05:58 +0700

    Page Module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { PagesRoutingModule } from './pages-routing.module';
-import { PopoverComponent } from './ui-elements/popover/popover.component';
-import { ProgressComponent } from './ui-elements/progress/progress.component';
 
 @NgModule({
-  declarations: [PopoverComponent, ProgressComponent],
+  declarations: [],
   imports: [CommonModule, PagesRoutingModule, CommonModule, NgbDropdownModule, PagesRoutingModule],
 })
 export class PagesModule {}
