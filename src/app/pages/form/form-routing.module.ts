5845ef292d4b687f4af4ddfe53100002db3f283a
Artithaya Jankul <artithaya.ja@gmail.com>
2020-03-06 16:57:56 +0700

    update form layouts

--- src/app/pages/form/form-routing.module.ts
+++ src/app/pages/form/form-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { FormComponent } from './form.component';
+import { FormElementsComponent } from './form-elements/form-elements.component';
+import { FormAdvancedComponent } from './form-advanced/form-advanced.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: FormComponent,
-  },
-  {
-    path: 'form',
-    component: FormComponent,
-  },
+  { path: 'form-elements', component: FormElementsComponent },
+  { path: 'form-layouts', component: FormAdvancedComponent },
 ];
 
 @NgModule({
