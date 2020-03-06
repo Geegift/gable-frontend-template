39439bd5fe9c9a313b1020a5c3d341fb85c16b3d
Artithaya Jankul <artithaya.ja@gmail.com>
2020-03-06 15:56:34 +0700

    update form element

--- src/app/pages/form/form-elements/form-elements.component.spec.ts
+++ src/app/pages/form/form-elements/form-elements.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FormComponent } from './form.component';
+import { FormElementsComponent } from './form-elements.component';
 
-describe('FormComponent', () => {
-  let component: FormComponent;
-  let fixture: ComponentFixture<FormComponent>;
+describe('FormElementsComponent', () => {
+  let component: FormElementsComponent;
+  let fixture: ComponentFixture<FormElementsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FormComponent ]
+      declarations: [ FormElementsComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FormComponent);
+    fixture = TestBed.createComponent(FormElementsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
